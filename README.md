1dd26d93c38e2188547c85e7e5899fd3fa343c05
mathischo <mathisschomacher@gmail.com>
2024-10-01 17:53:57 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# OpenInsider Scraper
+
+If you've always wanted to dive into the riveting world of insider trading data, but were too lazy to manually sift through thousands of pages, then buckle up because you're in for a treat. This is your magic carpet to travel through time, from 2013 to the present, and gather juicy tidbits of insider trading data from the future, well not really, only up to the current month (if it could gather data from the future, I'd probably be on my yacht in the Caribbean by now). 
+
+## What does this badboy do?
+
+The code in this repository makes use of the requests and BeautifulSoup libraries in Python to scrape data from openinsider. The results are neatly tucked away in a CSV file. I also added threading so it's as fast as a leopard.
+
+The script also comes with a built-in logger that logs events into a file because why not.
+
+## How to Run
+
+### Docker
+
+Simply build the image and run:
+
+```bash
+docker buildx build -t openinsider ./
+```
+
+```bash
+mkdir date
+docker run \
+-v "${PWD}/data":data \
+-e OUTPUT_DIR="data" \
+-it openinsider
+```
+
+You can also build the daily image and tell it when to start scraping:
+```bash
+docker buildx build -t openinsider-daily -f Dockerfile.daily
+```
+
+```bash
+mkdir data
+docker run \
+-v "${PWD}/data":data \
+-e OUTPUT_DIR="data" \
+-e START_DATE="2024-03-01" \
+-it openinsider-daily
+```
+
+### Bare python
+
+Running the script is as easy as a walk in the park... on a sunny day... with your favorite ice cream in your hand. Clone the repository, make sure you have the required libraries installed:
+```bash
+pip install --upgrade pip
+pip install requests BeautifulSoup4 logging datetime
+```
+and then just run the python script. Grab a cup of coffee, and watch it do the work. 
+
+## Disclaimer
+
+While this tool is quite powerful, it comes with no guarantee of making you rich. It will just make you *data* rich, which isn't necessarily the same thing. Also, this tool does not promote insider trading. It's called *insider trading data* scraper, not insider trading-data scraper.
+
+## Last words
+
+Enjoy the script and may the odds be ever in your favor!
---
+++ openinsider_scraper.py
@@ -0,0 +1,73 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+from concurrent.futures import ThreadPoolExecutor
+import logging
+import os
+from datetime import datetime, timedelta
+
+logger = logging.getLogger("my_logger")
+logger.setLevel(logging.WARNING)
+logger.addHandler(logging.FileHandler("logs.txt"))
+
+OUTPUT_DIR = os.environ.get('OUTPUT_DIR', 'data')
+
+def get_data_for_month(year, month):
+    start_date = datetime(year, month, 1).strftime('%m/%d/%Y')
+    end_date = (datetime(year, month, 1) + timedelta(days=32)).replace(day=1) - timedelta(days=1)
+    end_date = end_date.strftime('%m/%d/%Y')
+    print(f"processing month: {month}-{year}")
+    data = set()
+    url = (f'http://openinsider.com/screener?s=&o=&pl=&ph=&ll=&lh=&fd=-1&fdr={start_date}+-+'
+           f'{end_date}&td=0&tdr=&fdlyl=&fdlyh=&daysago=&xp=1&xs=1&vl=&vh=&ocl=&och=&sic1=-1&sicl=100&sich=9999&grp=0'
+           f'&nfl=&nfh=&nil=&nih=&nol=&noh=&v2l=&v2h=&oc2l=&oc2h=&sortcol=0&cnt=5000&page=1')
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    try:
+        rows = soup.find('table', {'class': 'tinytable'}).find('tbody').findAll('tr')
+    except:
+        print("Error! Skipping")
+        logger.error(f"This URL was not successful: {url}")
+        return
+
+    for row in rows:
+        cols = row.findAll('td')
+        if not cols:
+            continue
+        insider_data = {key: cols[index].find('a').text.strip() if cols[index].find('a') else cols[index].text.strip() 
+                        for index, key in enumerate(['Symbol', 'Filing Date', 'Trade Date', 'Ticker', 'Company Name',
+                                                     'Insider Name', 'Title', 'Trade Type', 'Price', 'Qty', 'Owned',
+                                                     'DeltaOwn', 'Value', '1d', '1w', '1m', '6m'])}
+        data.add(tuple(insider_data.values()))
+    return data
+
+def get_openinsider_data():
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        all_data = []
+        current_year = datetime.now().year
+        current_month = datetime.now().month
+        for year in range(2013, current_year + 1):
+            start_month = 1 if year != 2024 else 3
+            end_month = current_month if year == current_year else 12
+            for month in range(start_month, end_month + 1):
+                futures = [executor.submit(get_data_for_month, year, month)]
+                for future in futures:
+                    all_data.extend(future.result())
+                print(f"Done with {month}-{year}")
+
+        field_names = ['Symbol', 'Filing Date', 'Trade Date', 'Ticker', 'Company Name', 'Insider Name', 'Title',
+                       'Trade Type', 'Price', 'Qty', 'Owned', 'DeltaOwn', 'Value', '1d', '1w', '1m', '6m']
+
+        # join filename with output directory
+        output_file = os.path.join(OUTPUT_DIR, 'output_all_dates_monthly.csv')
+
+        with open(output_file, 'w', newline='') as f:
+            print("writing")
+            csv.writer(f).writerow(field_names)
+            for transaction in all_data:
+                csv.writer(f).writerow(transaction)
+
+# Execute the function
+get_openinsider_data()
+print("Completed Process.")
